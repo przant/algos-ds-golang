518c7dce79be07aa1d64e302ddbe29c05e3d8835
Antonio Pérez <apema2079@gmail.com>
2022-04-05 11:51:24 -0500

    Add test to check the function behavior

---
+++ 08-remove-adjacent-duplicates/remove_adjacent_duplicates_test.go
@@ -0,0 +1,37 @@
+package remove_duplicates
+
+import "testing"
+
+func TestRemoveDuplicates(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{
+			input: "aa",
+			want:  "",
+		},
+		{
+			input: "abbaca",
+			want:  "ca",
+		},
+		{
+			input: "azxxzy",
+			want:  "ay",
+		},
+		{
+			input: "aaabccddd",
+			want:  "abd",
+		},
+		{
+			input: "Mississippi",
+			want:  "M",
+		},
+	}
+
+	for _, test := range tests {
+		if got := removeDuplicates(test.input); got != test.want {
+			t.Errorf("with input %q, want %q but got %q", test.input, test.want, got)
+		}
+	}
+}
