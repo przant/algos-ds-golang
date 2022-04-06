6040459f817560e3c07fd589e717d23e3bb6e641
Antonio Pérez <apema2079@gmail.com>
2022-04-06 13:19:35 -0500

    Add test cases to check print numbers recursive functions

--- 10-recursion-basics/recursion_basics_test.go
+++ 10-recursion-basics/recursion_basics_test.go
@@ -1,6 +1,9 @@
 package recursion_basics
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestFactorial(t *testing.T) {
 	tests := []struct {
@@ -101,5 +104,81 @@ func TestPower(t *testing.T) {
 		if got := power(test.input[0], test.input[1]); got != test.want {
 			t.Errorf("with input %v, want %d but got %d", test.input, test.want, got)
 		}
+	}
+}
+
+func TestPrintNumbersDesc(t *testing.T) {
+	var buff bytes.Buffer
+
+	tests := []struct {
+		input int
+		want  string
+	}{
+		{
+			input: 0,
+			want:  "",
+		},
+		{
+			input: 1,
+			want:  "1",
+		},
+		{
+			input: 3,
+			want:  "123",
+		},
+		{
+			input: 5,
+			want:  "12345",
+		},
+		{
+			input: 10,
+			want:  "12345678910",
+		},
+	}
+
+	for _, test := range tests {
+		printNumbersAsc(test.input, &buff)
+		if got := buff.String(); got != test.want {
+			t.Errorf("with input %d, want %q but got %q", test.input, test.want, got)
+		}
+		buff.Reset()
+	}
+}
+
+func TestPrintNumbersAsc(t *testing.T) {
+	var buff bytes.Buffer
+
+	tests := []struct {
+		input int
+		want  string
+	}{
+		{
+			input: 0,
+			want:  "",
+		},
+		{
+			input: 1,
+			want:  "1",
+		},
+		{
+			input: 3,
+			want:  "321",
+		},
+		{
+			input: 5,
+			want:  "54321",
+		},
+		{
+			input: 10,
+			want:  "10987654321",
+		},
+	}
+
+	for _, test := range tests {
+		printNumbersDesc(test.input, &buff)
+		if got := buff.String(); got != test.want {
+			t.Errorf("with input %d, want %q but got %q", test.input, test.want, got)
+		}
+		buff.Reset()
 	}
 }
