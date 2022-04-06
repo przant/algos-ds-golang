b45511bf86d4664e8fb2ee6951de470f54f8bfcd
Antonio Pérez <apema2079@gmail.com>
2022-04-06 12:16:36 -0500

    Add some tests to check power function

--- 10-recursion-basics/recursion_basics_test.go
+++ 10-recursion-basics/recursion_basics_test.go
@@ -69,3 +69,37 @@ func TestFibonacci(t *testing.T) {
 		}
 	}
 }
+
+func TestPower(t *testing.T) {
+	tests := []struct {
+		input [2]int // [2]int{x, n}
+		want  int
+	}{
+		{
+			input: [2]int{5, 0},
+			want:  1,
+		},
+		{
+			input: [2]int{5, 1},
+			want:  5,
+		},
+		{
+			input: [2]int{5, 2},
+			want:  25,
+		},
+		{
+			input: [2]int{7, 4},
+			want:  2401,
+		},
+		{
+			input: [2]int{10, 4},
+			want:  10000,
+		},
+	}
+
+	for _, test := range tests {
+		if got := power(test.input[0], test.input[1]); got != test.want {
+			t.Errorf("with input %v, want %d but got %d", test.input, test.want, got)
+		}
+	}
+}
