e1ef7d6ec762f195c03c36e041825897df435755
Antonio Pérez <apema2079@gmail.com>
2022-04-06 12:04:03 -0500

    Implement fibonacci recursive function

--- 10-recursion-basics/recursion_basics.go
+++ 10-recursion-basics/recursion_basics.go
@@ -9,3 +9,13 @@ func factorial(n int) int {
 
 	return n * smallAns
 }
+
+func fibonacci(n int) int {
+	if n == 2 || n == 1 {
+		return 1
+	}
+
+	smallAns := fibonacci(n-1) + fibonacci(n-2)
+
+	return smallAns
+}
